6ae06d424a1edd310dda2a6bc5c18f30d3887280
jbmxworld@gmail.com <jbmxworld@gmail.com>
2020-10-29 09:21:48 -0500

    Responsive version

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,7 @@
 import '../sass/app.scss'
+import './modules/tabs.js'
 
-const tab = document.querySelector('.tabs');
-const tabButtons = tab.querySelectorAll('[role="tab"]');
-const tabPanels = Array.from(tab.querySelectorAll('[role="tabpanel"]'));
+// const mediaQuery = window.matchMedia('(min-width: 768px)')
 
-function tabClickHandler(e) {
-	//Hide All Tabpane
-	tabPanels.forEach(panel => {
-		panel.hidden = 'true';
-	});
-	
-	//Deselect Tab Button
-	tabButtons.forEach( button => {
-		button.setAttribute('aria-selected', 'false');
-		button.setAttribute('tabindex', '-1');
-	});
-	
-	//Mark New Tab
-	e.currentTarget.setAttribute('aria-selected', 'true');
-	e.currentTarget.setAttribute('tabindex', '0');
-	
-	//Show New Tab
-	const { id } = e.currentTarget;
-	
-	const currentTab = tabPanels.find(
-		panel => panel.getAttribute('aria-labelledby') === id
-	);
-	
-	currentTab.hidden = false;
-}
-
-tabButtons.forEach(button => {
-	button.addEventListener('mouseenter', tabClickHandler);
-})
+// if (mediaQuery.matches) {
+// }
